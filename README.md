7ece711986c16a49f97a0bd5310ec1177a758044
jasonwalsh <2184329+jasonwalsh@users.noreply.github.com>
2022-10-26 21:58:44 -0400

    docs: include header in README

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 <!-- prettier-ignore-start -->
 <!-- BEGIN_TF_DOCS -->
+
+This module is intended to validate a JSON document against a [JSON schema](https://json-schema.org/). This module
+uses the [http](https://registry.terraform.io/providers/hashicorp/http/latest/docs) provider to issue a `POST`
+request to a [JSON Schema Validator HTTP API](https://assertible.com/json-schema-validation). If the status code of
+the response is not 200 OK, then Terraform raises an error.
+
 ```hcl
 module "validate" {
   source = "../.."
--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+/**
+ *
+ * This module is intended to validate a JSON document against a [JSON schema](https://json-schema.org/). This module
+ * uses the [http](https://registry.terraform.io/providers/hashicorp/http/latest/docs) provider to issue a `POST`
+ * request to a [JSON Schema Validator HTTP API](https://assertible.com/json-schema-validation). If the status code of
+ * the response is not 200 OK, then Terraform raises an error.
+ *
+ */
 data "http" "this" {
   url    = "https://assertible.com/json"
   method = "POST"
